4ac888cb58f0dda678990a0cc97378c230270a87
Daniel Bilbao <bilbaod12@gmail.com>
2020-06-18 22:51:42 +0200

    Commit

---
+++ Ejercicio 4/Ejercicio_4.py
@@ -0,0 +1,8 @@
+#colocando la solucion y el proyecto en el mismo directorio
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
+
+numero=int(input("Introduzca un numero entero para determinar si es par o impar: "))
+if numero%2==0 :
+    print("Numero PAR")
+else :
+    print("Numero IMPAR")
