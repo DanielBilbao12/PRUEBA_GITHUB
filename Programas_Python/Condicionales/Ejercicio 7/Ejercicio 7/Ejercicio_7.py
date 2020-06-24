5b9aec8731f10cce02fb83f23db1cd1e20ff42a0
Daniel <bilbaod12@gmail.com>
2020-06-24 12:43:55 +0200

    Ejercicio 7

---
+++ Programas_Python/Condicionales/Ejercicio 7/Ejercicio 7/Ejercicio_7.py
@@ -0,0 +1,21 @@
+#Los tramos impositivos para la declaración de la renta en un determinado país son los siguientes:
+#Menos de 10000€	5%
+#Entre 10000€ y 20000€	15%
+#Entre 200000€ y 35000€	20%
+#Entre 350000€ y 60000€	30%
+#Más de 60000€	45%
+#Escribir un programa que pregunte al usuario su renta anual y muestre por pantalla 
+#el tipo impositivo que le corresponde.
+
+renta= float (input("Introduzca el valor de su renta anual en euros (€): \n"))
+
+if renta<10000 :
+    print("El tipo impositivo que le corresponde es del 5%")
+elif renta>=10000 and renta<=20000 :
+    print("El tipo impositivo que le corresponde es del 15%")
+elif renta>=20000 and renta<=35000 :
+    print("El tipo impositivo que le corresponde es del 20%")
+elif renta>=35000 and renta<=60000 :
+    print("El tipo impositivo que le corresponde es del 30%")
+elif renta>60000 :
+    print("El tipo impositivo que le corresponde es del 45%")
