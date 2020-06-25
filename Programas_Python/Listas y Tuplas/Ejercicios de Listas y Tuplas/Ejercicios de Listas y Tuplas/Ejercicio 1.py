f26e9f2b6c729f77fc85dac1985f5ea66c1d691e
Daniel Bilbao <bilbaod12@gmail.com>
2020-06-25 16:13:35 +0200

    Ejercicios de Listas y Tuplas en la misma solucion

--- Programas_Python/Listas y Tuplas/Ejercicios de Listas y Tuplas/Ejercicios de Listas y Tuplas/Ejercicio 1.py
+++ Programas_Python/Listas y Tuplas/Ejercicios de Listas y Tuplas/Ejercicios de Listas y Tuplas/Ejercicio 1.py
@@ -1,1 +1,8 @@
+#Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Informatica) 
+#en una lista y la muestre por pantalla.
 
+#NOTA: Esta solucion va a almacenar todos los ejercicios, a diferencia de los demas ejercicios que hemos usado una solucion para cada uno.
+#Para ejecutar los diferentes ejercicios hay que ir al Explorador de Archivos/Encima del ejercicio boton drch/Establecer como archivo ed inicio.
+#Seguramente haya alguna otra manera mas sencilla de compilarlo pero todabia no lo he descubierto.
+asignaturas= ["Matematicas", "Fisica", "Quimica", "Historia", "Informatica"]
+print (asignaturas)
